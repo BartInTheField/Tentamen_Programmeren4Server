07ebfd8998c6bcacfee2ae9524bf5e23b9540f5a
Niels van Dam <nielsvdam@hotmail.com>
2017-06-13 12:17:14 +0200

    Created files for films and rentals. TODO: implement logic.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var app = express();
 app.set('port', process.env.PORT || 8080);
 
 app.use('/api/v1', require('./routes/api/v1/login'));
+app.use('/api/v1', require('./routes/api/v1/register'));
+app.use('/api/v1', require('./routes/api/v1/films'));
+app.use('/api/v1', require('./routes/api/v1/rentals'));
+
 
 app.listen(app.get('port'), function(){
    console.log('Server is running on ' + app.get('port'));
