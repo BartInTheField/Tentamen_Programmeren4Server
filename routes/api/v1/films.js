78290ad275f1a44e2eb2435cc95b41cca72fe5c2
nielss94 <nielsvdam@hotmail.com>
2017-06-15 21:23:58 +0200

    Films endpoints works partially. Only '/films' works.

--- routes/api/v1/films.js
+++ routes/api/v1/films.js
@@ -3,8 +3,53 @@
  */
 var express = require('express');
 var router = express.Router();
+var auth = require('../../../auth/authentication');
 
 var pool = require('../../../db/connection');
 
+
+router.route('/films')
+    .get(function(req,res){
+        var start = req.query.offset;
+        var count = req.query.count;
+
+        var queryString = '';
+        if(start && count){
+            queryString = "SELECT * FROM `film` LIMIT "+count+" OFFSET "+start+";";
+        }else if(count){
+            queryString = "SELECT * FROM `film` LIMIT "+count+";";
+        }else{
+            queryString = "SELECT * FROM `film`";
+        }
+
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(err);
+            } else {
+                connection.query(queryString, function (err, rows) {
+                    connection.release();
+                    if(err){
+                        console.log(err);
+                        res.status(401);
+                    }else{
+                        if(rows.length > 0){
+                            res.status(200).json({
+                                "Movies" : rows
+                            });
+                        }else{
+                            res.status(400).json({
+                                "Response" : "Bad request"
+                            });
+                        }
+                    }
+                })
+            }
+        });
+    });
+
+router.route('/films/:filmid')
+    .get(function(req,res){
+
+    });
 
 module.exports = router;
